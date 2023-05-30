c02774963298ef2d4c3a549fe6044fb468c05806
yannick.hugues <yannhweb@gmail.com>
2023-05-30 17:36:06 +0200

    saisie lettre

--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"fmt"
 	"hangman/hangman"
+	"os"
 )
 
 func main() {
 	g := hangman.New(8, "Golang")
 	fmt.Println(g)
+
+	l, err := hangman.ReadGuess()
+	if err != nil {
+		fmt.Printf("Could not read from terminal: %v", err)
+		os.Exit(1)
+	}
+	fmt.Println(l)
 }
